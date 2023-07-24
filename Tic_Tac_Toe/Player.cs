c177e73cacc9879c12b8db00874085560ea0b01e
RuslanPidhainyi <ruslanpidgauny@gmail.com>
2023-07-24 18:55:15 +0200

    II - finaly

---
+++ Tic_Tac_Toe/Player.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tic_Tac_Toe
+{
+
+    interface IMoving
+    {
+        bool MakeMove(char[,] startBoard, char[,] gameBoard);
+    }
+
+
+    abstract class generalPlay
+    {
+        public string Name { get; set; }
+        public char Flage { get; set; }
+
+
+        public bool checkIfPlayerWon(char[,] gameBoard)
+        {
+            int heightY = gameBoard.GetLength(0);
+            int widthX = gameBoard.GetLength(1);
+
+            if (heightY != widthX)
+                throw new Exception("The board is not square!"); ;
+
+
+
+            // Check rows
+            for (int y = 0; y < heightY; y++)
+            {
+                int rowSum = 0;
+
+                for (int x = 0; x < widthX; x++)
+                {
+                    if (gameBoard[y, x] == Flage)
+                        rowSum++;
+                }
+                if (rowSum == widthX)
+                    return true;
+            }
+
+
+            // Check colms
+            for (int x = 0; x < widthX; x++)
+            {
+                int colSum = 0;
+
+                for (int y = 0; y < heightY; y++)
+                {
+                    if (gameBoard[y, x] == Flage)
+                        colSum++;
+                }
+                if (colSum == heightY)
+                    return true;
+            }
+
+
+            //Check diagonals
+            int diagSumA = 0;
+            int diagSumB = 0;
+
+
+            for (int z = 0; z < widthX; z++)
+            {
+                if (gameBoard[z, z] == Flage)
+                    diagSumA++;
+                if (gameBoard[z, widthX - 1 - z] == Flage)
+                    diagSumB++;
+            }
+
+            if (diagSumA == widthX || diagSumB == widthX)
+                return true;
+
+            //Else, no win
+            return false;
+        }
+
+        public bool PlaceSymbol(char n, char[,] startBoard, char[,] gameBoard)
+        {
+            int heightY = gameBoard.GetLength(0);
+            int widthX = gameBoard.GetLength(1);
+
+            if (heightY != startBoard.GetLength(0) || widthX != startBoard.GetLength(1))
+                throw new Exception("The boards have different sizes!");
+
+            for (int y = 0; y < heightY; y++)
+                for (int x = 0; x < widthX; x++)
+                    if ((gameBoard[y, x] == n) && (gameBoard[y, x] == startBoard[y, x]))
+                    {
+                        gameBoard[y, x] = Flage;
+                        return true;
+                    }
+
+            // else, return unsuccess
+            return false;
+        }
+    }
+
+
+
+    class HumanPlayer : generalPlay, IMoving
+    {
+        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
+        {
+            //Human: move
+            char chosenPlacePlayer;
+
+            do
+            {
+                Console.Write("Choose an empty place: ");
+                chosenPlacePlayer = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+            }
+            while (!PlaceSymbol(chosenPlacePlayer, startBoard, gameBoard));
+
+            return checkIfPlayerWon(gameBoard);
+        }
+    }
+
+    class AI : generalPlay, IMoving
+    {
+        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
+        {
+            //AI: move
+            Random rnd = new Random();
+            char chosenPlaceAI;
+
+            do
+            {
+                int p = rnd.Next(1, gameBoard.Length + 1); // random 1-9
+                chosenPlaceAI = p.ToString()[0];
+            }
+            while (!PlaceSymbol(chosenPlaceAI, startBoard, gameBoard));
+
+            Thread.Sleep(2000);
+
+            return checkIfPlayerWon(gameBoard);
+        }
+    }
+
+}
--- Tic_Tac_Toe/Program.cs
+++ Tic_Tac_Toe/Program.cs
@@ -7,54 +7,51 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            //bool start = true;
+            bool start = true;
 
-            //do
-            //{
-
-
-            //    Board.DrawBoard();
-
-            //    Game.Round();
-
-            //    Console.WriteLine("\nJeżelei chcesz wyjść napisz \"x\", jeżeli  chcesz kontynować klikni \"Enter\" \n");
-            //    start = Console.ReadLine() == "x";
-            //}
-            //while (!start);
-
-            HumanPlayer you = new HumanPlayer();
-            AI bot = new AI();
-
-            you.Name = "Player";
-            bot.Name = "PC";
-            you.Flage = 'x';
-            bot.Flage = 'o';
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            char[,] startBorad =
+            do
             {
-                { '1', '2', '3' },
-                { '4', '5', '6' },
-                { '7', '8', '9' }
-            };
+                Console.Clear();
+
+                HumanPlayer you = new HumanPlayer();
+                AI bot = new AI();
+
+                Console.WriteLine("Please, entre your name and click button\"Enter\". ");
+                you.Name = Console.ReadLine(); //"Player";
+
+                Console.Clear();
+                Console.WriteLine($"Thank you {you.Name}! Have a nice game)");
+                Thread.Sleep(2000);
+
+                bot.Name = "PC";
+                you.Flage = 'x';
+                bot.Flage = 'o';
+
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                char[,] startBorad =
+                {
+                    { '1', '2', '3' },
+                    { '4', '5', '6' },
+                    { '7', '8', '9' }
+                };
 
 
-            //gameBord = Dublication startBoard
-            char[,] gameBoard = startBorad.Clone() as char[,];
+                //gameBord = Dublication startBoard
+                char[,] gameBoard = startBorad.Clone() as char[,];
 
            
 
-           bool Player1_Won = false;
-           bool Player2_Won = false;
-           bool PlayerIsNext = true; // true - player 1 move,
-                                     // false - player 2 move
+                bool Player1_Won = false;
+                bool Player2_Won = false;
+                bool PlayerIsNext = true; // true - player 1 move,
+                                          // false - player 2 move
 
-            //////////////////////////////////////////////////////////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////////////////////////////////////////
 
-            //One move = One Round
+                //One move = One Round
             
             
                 for (int round = 0; round < gameBoard.Length; round++)
@@ -64,15 +61,15 @@ namespace Tic_Tac_Toe
 
                     if (PlayerIsNext) //player 1 move,
                     {
-                        Console.WriteLine(you.Name + " move");
+                        Console.WriteLine($"{you.Name} move");
                         Player1_Won = you.MakeMove(startBorad, gameBoard);
 
                         PlayerIsNext = false;
                     }
                     else //player 2 move,
                     {
-                        Console.WriteLine(you.Name + " move");
-                        Player2_Won = you.MakeMove(startBorad, gameBoard);
+                        Console.WriteLine($"{bot.Name} move");
+                        Player2_Won = bot.MakeMove(startBorad, gameBoard);
 
                         PlayerIsNext = true;
                     }
@@ -82,12 +79,24 @@ namespace Tic_Tac_Toe
                         break;
                 }
 
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            // End the game
-            Console.Clear();
-            DrawBoard(gameBoard);
-            Console.Write("Game ended! ");
+                // End the game
+                Console.Clear();
+                DrawBoard(gameBoard);
+                Console.Write("Game ended! ");
+
+                if (Player1_Won)
+                    Console.WriteLine($"Winner: {you.Name}");
+                else if (Player2_Won)
+                    Console.WriteLine($"Winner: {bot.Name}");
+                else Console.WriteLine("A tie");
+
+
+                Console.WriteLine("\nIf you want close game \"x\", or if you want continue click button \"Enter\" \n");
+                start = Console.ReadLine() == "x";
+            }
+            while (!start);
         }
 
 
@@ -98,104 +107,13 @@ namespace Tic_Tac_Toe
             for (int y = 0; y < board.GetLength(0); y++)
             {
                 for (int x = 0; x < board.GetLength(1); x++)
-                {
                     Console.Write(board[y, x]);
-                }
                 Console.WriteLine("\n");
             }
         }
     }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////
-
-    interface IMoving
-    {
-        bool MakeMove(char[,] startBoard, char[,] gameBoard);
-    }
-
-
-    abstract public class generalPlay
-    {
-        public  string Name { get; set; }
-        public  char Flage { get; set; }
+    
 
     
-        public  bool checkIfPlayerWon(char[,] gameBoard)
-        {
-            int heightY = gameBoard.GetLength(0);
-            int widthX = gameBoard.GetLength(1);
-
-            if (heightY != widthX)
-                //throw new Exception("The board is not square!"); ;
-
-
-
-            // Check rows
-            for (int y = 0; y < heightY; y++)
-            {
-                int rowSum = 0;
-
-                for (int x = 0; x < widthX; x++)
-                {
-                    if (gameBoard[y, x] == Flage)
-                        rowSum++;   
-                }
-                if (rowSum == widthX)
-                    return true;
-            }
-
-
-            // Check colms
-            for(int x=0; x < widthX;x++)
-            {
-                int colSum = 0;
-
-                for(int y = 0; y<heightY; y++)
-                {
-                    if (gameBoard[y, x] == Flage)
-                        colSum++;
-                }
-                if(colSum == heightY)
-                    return true;
-            }
-
-
-            //Check diagonals
-            int diagSumA = 0;
-            int diagSumB = 0;
-
-
-            for (int z = 0; z < widthX; z++)
-            {
-                if (gameBoard[z, z] == Flage)
-                    diagSumA++;
-                if (gameBoard[z, widthX - 1 - z] == Flage)
-                    diagSumB++;
-            }
-
-            if (diagSumA == widthX || diagSumB == widthX)
-                return true;
-
-            //Else, no win
-            return false;   
-        }
-    }
-
-    class HumanPlayer : generalPlay, IMoving
-    {
-        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
-        {
-            //Human: move
-            return checkIfPlayerWon(gameBoard);
-        }
-    }
-
-    class AI : generalPlay, IMoving
-    {
-        public bool MakeMove(char[,] startBoard, char[,] gameBoard)
-        {
-            //AI: move
-            return checkIfPlayerWon(gameBoard);
-        }
-    }
 }
